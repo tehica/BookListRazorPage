5fcbe1dcbca027c930a6947e79b3491ba94075be
tehica <tehica97@gmail.com>
2020-01-12 12:09:09 -0800

    First commit

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            // if modelstate is valid we want to add Book object to the DB
+            if (ModelState.IsValid)
+            {
+                // add book
+                await _db.Book.AddAsync(Book);
+
+                // after we add book, save the DB
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
